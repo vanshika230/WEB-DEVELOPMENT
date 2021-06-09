9a702d8d8efda21bf3ebaf226357ff1f1f05b747
vanshika230 <74042272+vanshika230@users.noreply.github.com>
2021-06-09 12:22:28 +0530

    Update fn.js

--- Interview Javascript/fn.js
+++ Interview Javascript/fn.js
@@ -6,6 +6,7 @@ function fn(){
 fn();
 //output :- 20
 
+//2. lexical scoping
 console.log("line number 1", varname);
 var varname=10;
 //fn b definition
@@ -26,3 +27,29 @@ line number 7 10
 line number 9 undefined
 line number 3 10
 line number 12 20 */
+
+//3. lexical scoping
+console.log("line number 1", varname);
+var varname=10;
+
+console.log("line number 7",varname);
+function fn(){
+    console.log("line number 9",varname);
+    var varname=20;
+    //fn b definition
+function b(){
+    console.log("line number 3",varname);
+       }
+    b();//function call
+    console.log("line number 12",varname);
+}
+fn(); 
+/* output :-
+line number 1 undefined
+line number 7 10
+line number 9 undefined
+line number 3 20
+line number 12 20
+*/
+
+
