96ed5302802355c6e620e9c6b6ed8c31009c0ca9
vanshika230 <74042272+vanshika230@users.noreply.github.com>
2021-06-09 13:36:17 +0530

    Update varques.js

--- Interview Javascript/varques.js
+++ Interview Javascript/varques.js
@@ -1,19 +1,19 @@
-// var a=10;
-// console.log("line number 2",a);
-// function fn(){
-//     console.log("line number 4",a);
-//     var a=20;
-//     a++;
-//     console.log("line number 7",a);
-//      if(a){
-//          var a=30;
-//          a++;
-//          console.log("line number 11",a);
-//      }
-//      console.log("line number 13",a);
-// }
-// fn();
-// console.log("line number 2",a);
+ var a=10;
+console.log("line number 2",a);
+ function fn(){
+     console.log("line number 4",a);
+     var a=20;
+     a++;
+     console.log("line number 7",a);
+      if(a){
+          var a=30;
+          a++;
+          console.log("line number 11",a);
+      }
+      console.log("line number 13",a);
+ }
+ fn();
+ console.log("line number 2",a);
 
 /*   output:- I expected
 line number 2 10
