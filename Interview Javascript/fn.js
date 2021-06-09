6761b9d21ef0f523e22987f962e6b906088b9942
vanshika230 <74042272+vanshika230@users.noreply.github.com>
2021-06-09 12:19:55 +0530

    Update fn.js

--- Interview Javascript/fn.js
+++ Interview Javascript/fn.js
@@ -4,3 +4,25 @@ function fn(){
     console.log(varname);
 }
 fn();
+//output :- 20
+
+console.log("line number 1", varname);
+var varname=10;
+//fn b definition
+function b(){
+    console.log("line number 3",varname);
+}
+console.log("line number 7",varname);
+function fn(){
+    console.log("line number 9",varname);
+    var varname=20;
+    b();//function call
+    console.log("line number 12",varname);
+}
+fn(); 
+/* output:-
+line number 1 undefined
+line number 7 10
+line number 9 undefined
+line number 3 10
+line number 12 20 */
